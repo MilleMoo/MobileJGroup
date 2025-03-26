c338dbadb893bdab45e6566225007525728319e9
Earkinwza007 <phasakorn.c@ku.th>
2025-03-26 16:59:33 +0700

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import CheckTransScreen from "./src/screens/CheckTransScreen";
+import RegisForm from "./src/screens/RegisForm";
+
+const Stack = createStackNavigator()
+
+const App = ()=>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+      name= "Home"
+      component={HomeScreen}
+      options={{ title:"Home Screen  "}}
+      />
+      <Stack.Screen
+      name= "Login"
+      component={LoginScreen}
+      options={{ title:"Login Screen  "}}
+      />
+      <Stack.Screen
+      name= "Register"
+      component={RegisForm}
+      options={{ title:"Register Screen  "}}
+      />
+      <Stack.Screen
+      name= "CheckTransScreen"
+      component={CheckTransScreen}
+      options={{ title:"CheckTransScreen  "}}
+      />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
