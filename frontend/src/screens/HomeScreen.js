76b99009fde3d4bf5fd1e9a08ac333840bb16c23
Earkinwza007 <phasakorn.c@ku.th>
2025-03-27 14:53:36 +0700

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -20,8 +20,6 @@ const HomeScreen = ({ navigation }) => {
             <TouchableOpacity style={[styles.button, styles.signUpButton]} onPress={() => navigation.navigate("Register")}>
                 <Text style={[styles.buttonText, styles.signUpButtonText]}>สมัครสมาชิก</Text>
             </TouchableOpacity>
-
-            <Text style={styles.socialText}>เข้าสู่ระบบผ่านโซเชียลมีเดีย</Text>
         </LinearGradient>
     );
 };
